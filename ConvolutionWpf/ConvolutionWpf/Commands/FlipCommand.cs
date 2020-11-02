c8fd06a87a4c97b652e86aabe0033dc576097d54
vladimir.galchenkov <volga.ndr@gmail.com>
2020-11-02 22:07:32 +0300

    clean FlipCommand

--- ConvolutionWpf/ConvolutionWpf/Commands/FlipCommand.cs
+++ ConvolutionWpf/ConvolutionWpf/Commands/FlipCommand.cs
@@ -23,7 +23,7 @@ namespace ConvolutionWpf.Commands
             if (image == null)
                 return;
 
-            //todo
+            
 
             var pixels = new byte[image.PixelHeight * image.BackBufferStride];
             image.CopyPixels(pixels, image.BackBufferStride, 0);
@@ -31,50 +31,7 @@ namespace ConvolutionWpf.Commands
             var imageRes = new WriteableBitmap(2 * image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
             var resultPixels = new byte[imageRes.PixelHeight * imageRes.BackBufferStride];
 
-            var IM = (image.PixelHeight - 1) * imageRes.BackBufferStride + 4 * (image.PixelWidth - 1);
-
-            for (int i = 0; i < image.PixelWidth; ++i)
-            {
-                for (int j = 0; j < image.PixelHeight; ++j)
-                {
-                    var indexRead = j * image.BackBufferStride + 4 * i;
-                    var indexWrite = j * imageRes.BackBufferStride + 4 * i;
-
-                    for (int c = 0; c < 4; ++c)
-                    {
-                        resultPixels[indexWrite + c] = pixels[pixels.Length - 4 - indexRead + c];
-                    }
-                }
-            }
-
-
-            for (int i = 0; i < image.PixelWidth; ++i)
-            {
-                for (int j = 0; j < image.PixelHeight; ++j)
-                {
-                    var indexRead = j * image.BackBufferStride + 4 * i;
-                    var indexWrite = 4 * image.PixelWidth + j * imageRes.BackBufferStride + 4 * i;
-
-                    for (int c = 0; c < 4; ++c)
-                    {
-                        resultPixels[indexWrite + c] = pixels[indexRead + c];
-                    }
-                }
-            }
-
-            //for (int i = 0; i < image.PixelWidth; ++i)
-            //{
-            //    for (int j = 0; j < image.PixelHeight; ++j)
-            //    {
-            //        var indexRead = j * image.BackBufferStride + 4 * i;
-            //        var indexWrite = j * imageRes.BackBufferStride + 4 * i;
-
-            //        for (int c = 0; c < 4; ++c)
-            //        {
-            //            resultPixels[indexWrite + c] = pixels[indexRead + c];
-            //        }
-            //    }
-            //}
+			//todo
 
             imageRes.WritePixels(new Int32Rect(0, 0, imageRes.PixelWidth, imageRes.PixelHeight), resultPixels, imageRes.BackBufferStride, 0);
 
